60505954cc12d7d08bb9e3674a025e4bab83d1e3
Elia Geretto <elia.f.geretto@gmail.com>
2021-07-02 17:31:28 +0200

    Refactor task_data.c

--- src/task_data.c
+++ src/task_data.c
@@ -1,80 +1,82 @@
 #include "task_data.h"
-#include <linux/slab.h>
 #include "debug.h"
+
+#include <linux/slab.h>
 
 static LIST_HEAD(task_data_list);
 static DEFINE_SPINLOCK(task_data_lock);
 
-static void task_data_free_callback(struct rcu_head *rcu) {
+static void task_data_free_callback(struct rcu_head *rcu)
+{
+	struct task_data *data = container_of(rcu, struct task_data, rcu);
+	struct vmrange_node *node;
 
-  struct task_data *data = container_of(rcu, struct task_data, rcu);
-  struct vmrange_node *n = data->blocklist;
-  while (n) {
-    data->blocklist = n->next;
-    kfree(n);
-    n = data->blocklist;
-  }
-  data->blocklist = NULL;
-  n = data->allowlist;
-  while (n) {
-    data->allowlist = n->next;
-    kfree(n);
-    n = data->allowlist;
-  }
-  data->allowlist = NULL;
+	DBG_PRINT("dropping task_data: %p\n", data);
 
-  kfree(data);
+	node = data->blocklist;
+	while (node) {
+		data->blocklist = node->next;
+		kfree(node);
+		node = data->blocklist;
+	}
+	data->blocklist = NULL;
+
+	node = data->allowlist;
+	while (node) {
+		data->allowlist = node->next;
+		kfree(node);
+		node = data->allowlist;
+	}
+	data->allowlist = NULL;
+
+	kfree(data);
 }
 
-struct task_data *get_task_data(const struct task_struct *tsk) {
-  struct task_data *data = NULL;
+struct task_data *get_task_data(const struct task_struct *tsk)
+{
+	struct task_data *data = NULL;
 
-  rcu_read_lock();
-  list_for_each_entry_rcu(data, &task_data_list, list) {
+	rcu_read_lock();
+	list_for_each_entry_rcu (data, &task_data_list, list) {
+		if (data->tsk == tsk) {
+			rcu_read_unlock();
+			return data;
+		}
+	}
+	rcu_read_unlock();
 
-    if (data->tsk == tsk) {
-
-      rcu_read_unlock();
-      return data;
-
-    }
-
-  }
-
-  rcu_read_unlock();
-
-  return NULL;
-
+	return NULL;
 }
 
-struct task_data *ensure_task_data(const struct task_struct *tsk) {
+struct task_data *ensure_task_data(const struct task_struct *tsk)
+{
+	struct task_data *data = NULL;
 
-  struct task_data *data = get_task_data(tsk);
-  if (data) return data;
+	data = get_task_data(tsk);
+	if (data)
+		return data;
 
-  // XXX: this is academic code (tm) so if we run out of memory, too bad!
-  // TODO: Not sure if this is still the case? should be freed correctly.
-  data = kmalloc(sizeof(struct task_data), GFP_KERNEL | __GFP_ZERO);
-  if (!data) return NULL;
+	data = kzalloc(sizeof(struct task_data), GFP_KERNEL);
+	if (!data) {
+		FATAL("allocation of new task_data struct failed!\n");
+		return NULL;
+	}
 
-  data->tsk = tsk;
-  INIT_LIST_HEAD(&data->ss.dirty_pages);
+	data->tsk = tsk;
+	INIT_LIST_HEAD(&data->ss.dirty_pages);
 
-  spin_lock(&task_data_lock);
-  list_add_rcu(&data->list, &task_data_list);
-  spin_unlock(&task_data_lock);
+	spin_lock(&task_data_lock);
+	list_add_rcu(&data->list, &task_data_list);
+	spin_unlock(&task_data_lock);
 
-  return data;
-
+	return data;
 }
 
-void remove_task_data(struct task_data *data) {
+void remove_task_data(struct task_data *data)
+{
+	spin_lock(&task_data_lock);
+	list_del_rcu(&data->list);
+	spin_unlock(&task_data_lock);
 
-  spin_lock(&task_data_lock);
-  list_del_rcu(&data->list);
-  spin_unlock(&task_data_lock);
-
-  call_rcu(&data->rcu, task_data_free_callback);
-
+	call_rcu(&data->rcu, task_data_free_callback);
 }
-
