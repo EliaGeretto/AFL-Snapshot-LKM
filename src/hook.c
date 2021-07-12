89fe80593700803d01475211b4e53c0e1434db7e
Elia Geretto <elia.f.geretto@gmail.com>
2021-07-12 13:22:40 +0200

    Refactor hook.c avoiding memory leaks

--- src/hook.c
+++ src/hook.c
@@ -6,60 +6,83 @@
 #include <linux/version.h>
 #include "debug.h"
 #include "ftrace_util.h"
-// TODO(andrea) switch from Kprobes to Ftrace
 
 struct hook {
-
-  struct kprobe    kp;
-  struct ftrace_ops fops;
-  struct list_head l;
-
+	char *symbol_name;
+	struct ftrace_ops fops;
+	struct list_head l;
 };
 
 LIST_HEAD(hooks);
 
-int try_hook(const char *func_name, void *handler) {
-  SAYF("Hooking function %s\n", func_name);
-  struct hook *hook = kmalloc(sizeof(struct hook), GFP_KERNEL | __GFP_ZERO);
-  INIT_LIST_HEAD(&hook->l);
-  hook->kp.symbol_name = func_name;
-  hook->kp.pre_handler = handler;
-  hook->fops.flags = FTRACE_OPS_FL_SAVE_REGS | FTRACE_OPS_FL_IPMODIFY | FTRACE_OPS_FL_RECURSION;
-  hook->fops.func = handler;
-  ftrace_set_filter(&hook->fops, func_name, strlen(func_name), 0);
-  int ret = register_ftrace_function(&hook->fops);
-  SAYF("Hooked function: %d\n", ret);
-  // int ret = register_kprobe(&hook->kp);
-  if (!ret) { list_add(&hook->l, &hooks); }
+int try_hook(char *func_name, void *handler)
+{
+	struct hook *hook;
+	int ret = 0;
 
-  return true;
+	SAYF("Hooking function %s\n", func_name);
+	hook = kzalloc(sizeof(struct hook), GFP_KERNEL);
+	if (!hook) {
+		ret = -ENOMEM;
+		goto err;
+	}
 
+	hook->symbol_name = func_name;
+	hook->fops.flags = FTRACE_OPS_FL_SAVE_REGS | FTRACE_OPS_FL_IPMODIFY |
+			   FTRACE_OPS_FL_RECURSION;
+	hook->fops.func = handler;
+	INIT_LIST_HEAD(&hook->l);
+
+	ret = ftrace_set_filter(&hook->fops, func_name, strlen(func_name), 0);
+	if (ret)
+		goto err_release;
+
+	ret = register_ftrace_function(&hook->fops);
+	if (ret)
+		goto err_release;
+
+	list_add(&hook->l, &hooks);
+
+	SAYF("Function hooked: %s\n", func_name);
+
+	return 0;
+
+err_release:
+	kfree(hook);
+
+err:
+	return ret;
 }
 
-void unhook(const char *func_name) {
+void unhook(const char *func_name)
+{
+	struct hook *hook = NULL;
+	struct hook *n = NULL;
+	int res;
 
-  struct hook *hook = NULL;
-  list_for_each_entry(hook, &hooks, l) {
-
-    if (!strcmp(hook->kp.symbol_name, func_name)) {
-
-      // unregister_kprobe(&hook->kp);
-      unregister_ftrace_function(&hook->fops);
-
-    }
-
-  }
-
+	list_for_each_entry_safe (hook, n, &hooks, l) {
+		if (!strcmp(hook->symbol_name, func_name)) {
+			res = unregister_ftrace_function(&hook->fops);
+			if (res)
+				WARNF("unregister_ftrace_function failed");
+			list_del(&hook->l);
+			kfree(hook);
+			break;
+		}
+	}
 }
 
-void unhook_all(void) {
+void unhook_all(void)
+{
+	struct hook *hook = NULL;
+	struct hook *n = NULL;
+	int res;
 
-  struct hook *hook = NULL;
-  list_for_each_entry(hook, &hooks, l) {
-
-    // unregister_kprobe(&hook->kp);
-    unregister_ftrace_function(&hook->fops);
-
-  }
-
+	list_for_each_entry_safe (hook, n, &hooks, l) {
+		res = unregister_ftrace_function(&hook->fops);
+		if (res)
+			WARNF("unregister_ftrace_function failed");
+		list_del(&hook->l);
+		kfree(hook);
+	}
 }
