822de920d0d4509b5456a26e18a4f9d2e2c64152
Elia Geretto <elia.f.geretto@gmail.com>
2021-12-13 10:13:08 +0100

    Improve test1.c

--- test/test1.c
+++ test/test1.c
@@ -5,29 +5,30 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-#include "afl_snapshot.h"
 #include "libaflsnapshot.h"
 
-bool test(uint8_t *shm_addr, uint8_t *none_addr, size_t page_size) {
+static bool test(uint8_t *shm_addr, uint8_t *priv_addr, size_t page_size) {
   *shm_addr = 0;
-  *none_addr = 0;
+  *priv_addr = 0;
 
-  fprintf(stderr, "shm_addr: %p, none_addr: %p\n", shm_addr, none_addr);
+  fprintf(stderr, "shm_addr: %p, priv_addr: %p\n", shm_addr, priv_addr);
 
-  if (afl_snapshot_take(AFL_SNAPSHOT_NOSTACK) == 1) { puts("Snapshot taken"); }
+  if (afl_snapshot_take(AFL_SNAPSHOT_NOSTACK) == 1) {
+    fputs("Snapshot taken\n", stderr);
+  }
 
   for (size_t idx = 0; idx < 10; idx++) {
-    *none_addr += 1;
-    *(none_addr + page_size) += 1;
+    *priv_addr += 1;
+    *(priv_addr + page_size) += 1;
     *shm_addr += 1;
     *(shm_addr + page_size) += 1;
 
-    fprintf(stderr, "shm_addr[0]: %d, none_addr[0]: %d\n", *shm_addr,
-            *none_addr);
-    fprintf(stderr, "shm_addr[1]: %d, none_addr[1]: %d\n",
-            *(shm_addr + page_size), *(none_addr + page_size));
+    fprintf(stderr, "shm_addr[0]: %d, priv_addr[0]: %d\n", *shm_addr,
+            *priv_addr);
+    fprintf(stderr, "shm_addr[1]: %d, priv_addr[1]: %d\n",
+            *(shm_addr + page_size), *(priv_addr + page_size));
 
-    puts("Restoring snapshot");
+    fputs("Restoring snapshot\n", stderr);
     afl_snapshot_restore();
   }
 
@@ -35,14 +36,14 @@ bool test(uint8_t *shm_addr, uint8_t *none_addr, size_t page_size) {
 
   if (*(shm_addr + page_size) != 10) { return false; }
 
-  if (*none_addr != 10) { return false; }
+  if (*priv_addr != 10) { return false; }
 
-  if (*(none_addr + page_size) != 0) { return false; }
+  if (*(priv_addr + page_size) != 0) { return false; }
 
   return true;
 }
 
-int main() {
+int main(void) {
   long page_size = sysconf(_SC_PAGESIZE);
   if (page_size == -1) {
     perror("Could not retrieve page size");
@@ -61,17 +62,28 @@ int main() {
     exit(1);
   }
 
-  uint8_t *none_addr = mmap(NULL, page_size * 2, PROT_READ | PROT_WRITE,
+  uint8_t *priv_addr = mmap(NULL, page_size * 2, PROT_READ | PROT_WRITE,
                             MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-  if (none_addr == MAP_FAILED) {
+  if (priv_addr == MAP_FAILED) {
     perror("Could not map private memory");
     exit(1);
   }
 
-  afl_snapshot_exclude_vmrange(none_addr, none_addr + page_size);
+  afl_snapshot_exclude_vmrange(priv_addr, priv_addr + page_size);
   afl_snapshot_include_vmrange(shm_addr, shm_addr + page_size);
 
-  if (!test(shm_addr, none_addr, page_size)) { exit(1); }
+  // priv_addr[0] is excluded manually
+  // priv_addr[1] is included by default
+  // shm_addr[0] is included manually
+  // shm_addr[1] is excluded by default
+
+  fputs("Only shm_addr[0] and priv_addr[1] should be restored.\n", stderr);
+
+  if (!test(shm_addr, priv_addr, page_size)) {
+    fputs("Failure!\n", stderr);
+    exit(1);
+  }
+  fputs("Success!\n", stderr);
 
   return 0;
 }
