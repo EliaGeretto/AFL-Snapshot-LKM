aa741b328dedcad7b1d1d1e7a0ba4b9f1d4eeeee
Elia Geretto <elia.f.geretto@gmail.com>
2021-12-13 10:58:33 +0100

    Improve test7.c

--- test/test7.c
+++ test/test7.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <sys/resource.h>
 
 #include "libaflsnapshot.h"
 
 int main(void) {
+  struct rlimit core_lim = {
+      .rlim_cur = 0,
+      .rlim_max = 0,
+  };
+  if (setrlimit(RLIMIT_CORE, &core_lim) == -1) {
+    perror("Could not set core limit");
+    exit(1);
+  }
+
   if (afl_snapshot_init() == -1) {
     perror("Initialization failed");
     exit(1);
@@ -26,6 +36,8 @@ int main(void) {
     puts("Restoring snapshot");
     afl_snapshot_restore();
   }
+
+  puts("The program should abort without kernel errors.");
 
   abort();
 
