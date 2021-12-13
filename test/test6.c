32a0a158065f1da6bbea9bdc1cbfeb484fcccec1
Elia Geretto <elia.f.geretto@gmail.com>
2021-12-13 10:53:19 +0100

    Improve test6.c

--- test/test6.c
+++ test/test6.c
@@ -7,8 +7,6 @@
 
 #include "libaflsnapshot.h"
 
-#define CHUNK_SIZE 5
-
 void *sleep_task(void *p) {
   puts("secondary thread sleeping for 3 seconds");
   sleep(3);
@@ -21,20 +19,9 @@ int main(void) {
     exit(1);
   }
 
-  int res_file = open("./res", O_RDONLY);
-  if (res_file < 0) {
-    perror("Could not open resource file");
-    return 1;
-  }
-
   puts("Spawning secondary thread");
   pthread_t secondary_thread;
   pthread_create(&secondary_thread, NULL, sleep_task, NULL);
-
-  char chunk[CHUNK_SIZE];
-  read(res_file, chunk, CHUNK_SIZE - 1);
-  chunk[CHUNK_SIZE - 1] = '\0';
-  printf("first chunk: \"%s\"\n", chunk);
 
   bool is_restored = false;
   if (afl_snapshot_take(AFL_SNAPSHOT_MMAP | AFL_SNAPSHOT_FDS |
@@ -46,10 +33,7 @@ int main(void) {
     is_restored = true;
   }
 
-  read(res_file, chunk, CHUNK_SIZE - 1);
-  printf("second chunk: \"%s\"\n", chunk);
-
-  close(res_file);
+  puts("Running main thread");
 
   if (!is_restored) {
     puts("Restoring snapshot");
